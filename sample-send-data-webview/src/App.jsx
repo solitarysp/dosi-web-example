39efc68828fcd7f33476f24c93b77fbe3a89a2fb
le.van.thanh <le.van.thanh@linecorp.com>
2023-09-08 15:27:48 +0700

    add sample-send-data-webview

--- sample-send-data-webview/src/App.jsx
+++ sample-send-data-webview/src/App.jsx
@@ -1,9 +1,10 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
 import SenDataToApp from "./SendDataToApp.jsx";
 import Header from "./Header.jsx";
 import './style.css';
+import Footer from "./Footer.jsx";
 
 function App() {
     return (
@@ -12,11 +13,11 @@ function App() {
                 <Header></Header>
                 <div className="wrapper">
                     <Switch>
-                        <Route path={"/"} exact component={SenDataToApp} />
-                        <Route path={"/send_data_to_app"} exact component={SenDataToApp} />
+                        <Route path={"/"} exact component={SenDataToApp}/>
+                        <Route path={"/send_data_to_app"} exact component={SenDataToApp}/>
                     </Switch>
                 </div>
-
+                <Footer/>
             </div>
         </Router>
     )
---
+++ sample-send-data-webview/src/Footer.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { NavLink } from "react-router-dom";
+import './style.css';
+
+function Footer() {
+    return (
+        <div className="footer">
+            <h4>Footer</h4>
+        </div>
+    )
+}
+
+export default Footer
