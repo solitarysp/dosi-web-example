8c214cf2bab6bdb588a82612524cf82898869382
le.van.thanh <le.van.thanh@linecorp.com>
2023-11-14 14:53:31 +0700

    ad

--- sample-send-data-webview/src/SendDataToApp.jsx
+++ sample-send-data-webview/src/SendDataToApp.jsx
@@ -128,7 +128,7 @@ function SenDataToApp() {
           <br />
         <a href={"https://dosivault.page.link/qL6j?uri_dapps=https://sample-send-data-webview-lv00212.website.line-apps-dev.com"}>Open this page by dynamic link</a>
         <br />
-        <a href={"https://dosivault.page.link/muUhj?uri_dapps=https://sample-send-data-webview-lv00212.website.line-apps-dev.com"}>Open this page by dynamic link beta</a>
+        <a href={"https://dosivault.page.link/muUh?uri_dapps=https://sample-send-data-webview-lv00212.website.line-apps-dev.com"}>Open this page by dynamic link beta</a>
 
         <br />
         <a href={"app.dosivault://dapp?uri_dapps= http://localhost:5173&efr=1"}>Open this page on dosi vault local</a>
